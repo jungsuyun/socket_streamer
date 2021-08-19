98990f7c0eb67509310cf88fb63b2ce0b8976620
jungsuyun <69963988+jungsuyun@users.noreply.github.com>
2021-08-20 01:18:05 +0900

    Update url_test.py

--- url_test.py
+++ url_test.py
@@ -1,12 +1,17 @@
 import urllib.request
-
+import io
 import cv2
 import numpy as np
 
-image_url = 'http://192.168.0.24:7079/hi'
+img_url = 'http://192.168.0.2:7079/hi'
 while True:
-    resp = urllib.request.urlopen(image_url)
-    nparr = np.frombuffer(resp.read(), np.byte)
-    image_raw = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    data = urllib.request.urlopen(img_url)
+    raw_data = data.read()
 
-    cv2.imshow("output", image_raw)
+    nparr = np.frombuffer(raw_data, np.byte)
+    image_raw = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
+    cv2.imshow("test", image_raw)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
